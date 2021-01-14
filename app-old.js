d43ad0bff5554bdb473a36a1f7effefc325cd46f
Juan <juanbaas48@gmail.com>
2021-01-13 20:28:28 -0600

    Primer commit

---
+++ app-old.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+
+http.createServer((req, res) => {
+
+        res.writeHead(200, { 'Content-Type': 'aplicaction/json' });
+
+        let salida = {
+                nombre: 'Juan',
+                edad: 23,
+                url: req.url
+            }
+            //res.write('Hola mundo');
+        res.write(JSON.stringify(salida));
+        res.end();
+
+    })
+    .listen(8080);
+
+
+console.log('Escuchando el puerto 8080');
